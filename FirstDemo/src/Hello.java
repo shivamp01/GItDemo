875544f6a31dbbc6b301b17815f1d1b7db08dd27
Shivam <shivampadmawar001@gmail.com>
2021-06-13 23:13:32 +0530

    second commit

--- FirstDemo/src/Hello.java
+++ FirstDemo/src/Hello.java
@@ -2,5 +2,7 @@
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		System.out.println("Bonjour le monde");
+		System.out.println("Hola Mundo");
 	}
 }
