652c6feb059b86a9c553f787b4670a93314a80b6
Shivam <shivampadmawar001@gmail.com>
2021-06-14 15:07:20 +0530

    branching commit

--- FirstDemo/src/Hello.java
+++ FirstDemo/src/Hello.java
@@ -1,5 +1,9 @@
 
 public class Hello {
+
+	public void getData(){
+		System.out.println("Inside getdata function");
+	}
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("Bonjour le monde");
